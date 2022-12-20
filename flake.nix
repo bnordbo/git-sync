d233218c44ba2f7b441a4e4316f3d57137a5eaf3
Bjørn Nordbø <bnordbo@proton.me>
2022-12-20 20:36:55 +0100

    Sensible overlay that does not work

--- flake.nix
+++ flake.nix
@@ -17,13 +17,17 @@
             buildCommand = "${old.buildCommand}\n patchShebangs $out";
           });
       in rec {
-        defaultPackage = packages.bn-git-sync;
+        packages.default = packages.bn-git-sync;
 
         packages.bn-git-sync = pkgs.symlinkJoin {
           name = name;
           paths = [ run-sync ] ++ [ pkgs.git ];
           buildInputs = [ pkgs.makeWrapper ];
           postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        };
+
+        overlay = final: prev: {
+          bn-git-sync = { bn-git-sync = self.packages.default; };
         };
       });
 }
