b96463cd6f277a8d45657cf93e6bd6bc42a8f248
Bjørn Nordbø <bnordbo@proton.me>
2022-12-22 19:27:54 +0100

    This actually seems to work, sort of

--- flake.nix
+++ flake.nix
@@ -3,31 +3,29 @@
 {
   description = "Sync git repos; adding all changes and pushing to upstream";
 
-  inputs = {
-    flake-utils.url = github:numtide/flake-utils;
-  };
+  outputs = { self, nixpkgs }:
+    let
+      name = "bn-git-sync";
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+      run-sync = (pkgs.writeScriptBin name (builtins.readFile ./run-sync.sh))
+        .overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+    in rec {
+      packages.${system} = rec {
+        default = bn-git-sync;
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        name = "bn-git-sync";
-        run-sync = (pkgs.writeScriptBin name (builtins.readFile ./run-sync.sh))
-          .overrideAttrs(old: {
-            buildCommand = "${old.buildCommand}\n patchShebangs $out";
-          });
-      in rec {
-        packages.default = packages.bn-git-sync;
-
-        packages.bn-git-sync = pkgs.symlinkJoin {
+        bn-git-sync = pkgs.symlinkJoin {
           name = name;
           paths = [ run-sync ] ++ [ pkgs.git ];
           buildInputs = [ pkgs.makeWrapper ];
           postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
         };
+      };
 
-        overlay = final: prev: {
-          bn-git-sync = { bn-git-sync = self.packages.default; };
-        };
-      });
+      overlays.default = final: prev: {
+        bn-git-sync = { bn-git-sync = packages.${system}.default; };
+      };
+    };
 }
